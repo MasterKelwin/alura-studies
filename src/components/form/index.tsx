e877d4779163d9e6be26d5a726a2ca6429cd5a6f
MasterKelwin <mkelwin@outlook.com>
2022-05-02 17:19:50 -0300

    utilizando uuid

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -2,19 +2,37 @@ import React from "react";
 import { ITarefa } from "../../types/tarefa";
 import Botao from "../button";
 import style from './form.module.scss';
+import { v4 as uuidv4 } from 'uuid';
 
 class Form extends React.Component<{
     setTarefas: React.Dispatch<ITarefa[] | any>  
 }> {
     state: ITarefa = {
         tarefa: "",
-        tempo: "00:00"
+        tempo: "00:00",
+        selecionado: false,
+        completado: false,
+        id: 'cu'
     }
 
 
     addTarefa(evento: React.FormEvent<HTMLFormElement>) {
         evento.preventDefault(); //para não dar refresh na página
-        this.props.setTarefas((tarefasAntigas: any) => [...tarefasAntigas, {...this.state}])
+        this.props.setTarefas((tarefasAntigas: any) =>
+        [
+            ...tarefasAntigas,
+            {
+                ...this.state,
+                selecionado: false,
+                completado: false,
+                id: uuidv4()
+            }
+        ]
+    );
+        this.setState({
+            tarefa: "",
+            tempo: "00:00"
+        })
     }
 
     render() {
